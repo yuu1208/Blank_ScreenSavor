f7a6d202fefa8f9b78473c8bc4ca5701be4cb501
ゆう <72594751+yuu1208@users.noreply.github.com>
2022-02-10 06:18:23 +0900

    階層ファイル削除して直下に投入

---
+++ Blank_ScreenSaverView.h
@@ -0,0 +1,13 @@
+//
+//  Blank_ScreenSaverView.h
+//  Blank_ScreenSaver
+//
+//  Created by 中根 優太 on 2022/02/10.
+//  https://github.com/yuu1208
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface Blank_ScreenSaverView : ScreenSaverView
+
+@end
